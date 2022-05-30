5ae81dd52c271e3c06c6c66c32f355508d114b52
nguyenvo <vbnguyen.dhsp@gmail.com>
2022-05-31 06:40:20 +0700

    add data view pane

--- js/testbench.js
+++ js/testbench.js
@@ -5,3 +5,15 @@ $(".ic-dropdown-item").bind("click", (e) => {
   dropdownValue.innerHTML = e.target.innerHTML;
   dropdownValue.setAttribute("value", e.target.getAttribute("value"));
 });
+
+// Tree view
+$(".symbol-expand")
+  .before()
+  .bind("click", (e) => {
+    treeBranch = e.target.closest(".tree-branch");
+    console.log(treeBranch);
+    treeBranch.querySelector(".tree-nested").classList.toggle("active");
+    e.target.classList.toggle("symbol-collapse");
+  });
+
+console.log($(".symbol-expand").before());
